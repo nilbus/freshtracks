4482acf7bc84ad13742ec9bc720890f336529c2a
Edward Anderson <nilbus@nilbus.com>
2009-11-25 18:07:21 -0500

    Define requirement's needs relationship.

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -1,2 +1,4 @@
 class Requirement < ActiveRecord::Base
+  belongs_to :course, :class_name => 'Course', :foreign_key => 'course_id'
+  belongs_to :needs, :class_name => 'Course', :foreign_key => 'needs_id'
 end
