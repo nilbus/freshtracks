6ad128fc2e1c82f1dfabf8c8e618dfc8267a1079
Edward Anderson <nilbus@nilbus.com>
2009-11-26 09:50:36 -0500

    Remove sections controller (index action) and index, since these are always viewed from inside a course.

--- app/controllers/sections_controller.rb
+++
@@ -1,6 +0,0 @@
-class SectionsController < ApplicationController
-  # sections for current semester
-  def index
-    @sections = Section.all(:order => "course_id") #(:conditions => ['semester_id = ', session[:semester]])
-  end
-end
