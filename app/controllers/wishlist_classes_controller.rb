c183f08d5188ebf95d71af561481900a6ae608b2
Ben Berry <bsberry@ncsu.edu>
2009-11-30 15:03:56 -0500

    Preliminary wishlist index

--- app/controllers/wishlist_classes_controller.rb
+++ app/controllers/wishlist_classes_controller.rb
@@ -2,11 +2,14 @@ class WishlistClassesController < ApplicationController
   # show wishlist
   def index
     @wishlist_classes = WishlistClass.all
+    section_ids = @wishlist_classes.map{ |c| c.section_id}
+    @wishlist_courses = Course.all(:joins => :sections, :conditions => ['sections.id in (?)', section_ids])   
   end
 
   # add a class to wishlist
   def create
     @wishlist_class = WishlistClass.new(params[:wishlist_class])
+    @wishlist_class.section_id = params[:id]
     @wishlist_class.save
   end
 
