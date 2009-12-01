c0828cd8251d0cbc91637adc582a54c2ecebf323
Edward Anderson <nilbus@nilbus.com>
2009-12-01 00:33:52 -0500

    Move course listing styles to application.css

--- app/controllers/degree_requirements_controller.rb
+++ app/controllers/degree_requirements_controller.rb
@@ -1,7 +1,6 @@
 class DegreeRequirementsController < ApplicationController
   # plan of work, with classes loaded
   def index
-    @degree_requirements = DegreeRequirements.all(:include => :classes)
+    @degree_requirements = DegreeRequirement.all(:include => :required)
   end
-
 end
