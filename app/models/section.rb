e2ca8f53a685a31ab655117b259f28c2bdec43e5
Ben Berry <bsberry@ncsu.edu>
2009-12-01 16:43:10 -0500

    Add timeslot to section model, use it in wishlist index

--- app/models/section.rb
+++ app/models/section.rb
@@ -3,4 +3,9 @@ class Section < ActiveRecord::Base
   belongs_to :semester
   has_many :wishlist_sections
   has_many :registered_sections
+  
+  def timeslot
+    time_format = '%I:%M%p'
+    begin_time.strftime(time_format) + "-" + end_time.strftime(time_format)
+  end
 end
