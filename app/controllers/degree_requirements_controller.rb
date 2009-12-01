51cdfa6a6f5076c294dce5172326f5bc2896aaf1
Edward Anderson <nilbus@nilbus.com>
2009-12-01 00:35:00 -0500

    Revert "Move course listing styles to application.css"
    This reverts commit c0828cd8251d0cbc91637adc582a54c2ecebf323.

--- app/controllers/degree_requirements_controller.rb
+++ app/controllers/degree_requirements_controller.rb
@@ -1,6 +1,7 @@
 class DegreeRequirementsController < ApplicationController
   # plan of work, with classes loaded
   def index
-    @degree_requirements = DegreeRequirement.all(:include => :required)
+    @degree_requirements = DegreeRequirements.all(:include => :classes)
   end
+
 end
