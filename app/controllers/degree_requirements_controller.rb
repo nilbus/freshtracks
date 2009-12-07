6237fa7ca1cd95a08c12a31ea054be4896a60644
Edward Anderson <nilbus@nilbus.com>
2009-12-07 10:43:28 -0500

    Don't display buttons for things already added

--- app/controllers/degree_requirements_controller.rb
+++ app/controllers/degree_requirements_controller.rb
@@ -2,5 +2,9 @@ class DegreeRequirementsController < ApplicationController
   # plan of work, with classes loaded
   def index
     @degree_requirements = DegreeRequirement.all(:include => :required)
+
+    # Load schedule so we don't display buttons for things already added
+    @registered_section_ids = RegisteredSection.all.map &:section_id
+    @wishlist_section_ids = WishlistSection.all.map &:section_id
   end
 end
