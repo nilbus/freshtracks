6c2cc20b8078db669b04d48215088d87d91f3ef4
Edward Anderson <nilbus@nilbus.com>
2009-12-07 01:23:49 -0500

    Wishlist and Add buttons check for duplicates, and give feedback.

--- app/models/registered_section.rb
+++ app/models/registered_section.rb
@@ -1,3 +1,5 @@
 class RegisteredSection < ActiveRecord::Base
   belongs_to :section
+
+  validates_uniqueness_of :section_id, :message => 'is already on your schedule.'
 end
--- app/models/wishlist_section.rb
+++ app/models/wishlist_section.rb
@@ -1,3 +1,5 @@
 class WishlistSection < ActiveRecord::Base
   belongs_to :section
+
+  validates_uniqueness_of :section_id, :message => 'is already on your wishlist.'
 end
