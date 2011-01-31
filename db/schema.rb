493ae61e2e6e94b0ca9d2af62d48778497b67ec0
Ben Berry <bsberry@ncsu.edu>
2011-01-31 11:58:07 -0500

    Add users to the schema and add test data for a sample user: student@ncsu.edu.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101228210645) do
+ActiveRecord::Schema.define(:version => 20110124160125) do
 
   create_table "courses", :force => true do |t|
     t.string   "name"
@@ -82,6 +82,19 @@ ActiveRecord::Schema.define(:version => 20101228210645) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "encrypted_password", :limit => 128
+    t.string   "salt",               :limit => 128
+    t.string   "confirmation_token", :limit => 128
+    t.string   "remember_token",     :limit => 128
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email"
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
   create_table "wishlist_sections", :force => true do |t|
     t.integer  "section_id"
