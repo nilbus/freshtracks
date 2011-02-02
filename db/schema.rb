82f36fc17b98dcf31d3c3d3d28c97f8cf754a583
Ben Berry <bsberry@ncsu.edu>
2011-02-02 11:39:54 -0500

    Add user_id to tables, add fixture users to fixtures.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110124160125) do
+ActiveRecord::Schema.define(:version => 20110202153559) do
 
   create_table "courses", :force => true do |t|
     t.string   "name"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(:version => 20110124160125) do
     t.integer  "course_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "prerequisites", :force => true do |t|
@@ -61,6 +62,7 @@ ActiveRecord::Schema.define(:version => 20110124160125) do
     t.integer  "section_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "sections", :force => true do |t|
@@ -100,6 +102,7 @@ ActiveRecord::Schema.define(:version => 20110124160125) do
     t.integer  "section_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
 end
