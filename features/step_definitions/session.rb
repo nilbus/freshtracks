479dbe62ac7bb4cba57bf7e6e112e9bb90799619
Edward Anderson <nilbus@nilbus.com>
2011-02-09 14:10:22 -0500

    Add cucumber features for authentication

--- features/step_definitions/session.rb
+++ features/step_definitions/session.rb
@@ -1,15 +1,18 @@
 Given /^I have logged in as an? "([^"]*)"$/ do |user_type|
-  pending unless user_type == 'student'
-  visit '/'
+  sign_in_as(user_type)
 end
 
 Given /^I have not logged in$/ do
-  # no action necessary
-  visit '/'
+  # nothing to do
 end
 
 When /^I log in as an? "([^"]*)"$/ do |user_type|
-  pending unless user_type == 'student'
-  visit '/'
+  sign_in_as(user_type)
 end
 
+def sign_in_as(user_type)
+  visit '/sign_in'
+  fill_in 'Email', :with => "#{user_type}@ncsu.edu"
+  fill_in 'Password', :with => 'password'
+  click_button 'Sign in'
+end
