679951d3baed3680ec6b9d7612b6b10180bc48fd
Edward Anderson <nilbus@nilbus.com>
2011-02-21 11:44:48 -0500

    Rewrite course search as Rails 3 AR query

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,27 +4,22 @@ class CoursesController < ApplicationController
   # search courses; returns matching
   # Currently only uses one search term
   def index
-    begin
-                                           # sqlite query
-      @courses = Course.all(:conditions => ['semester_id = ? AND (courses.subject || courses.number || \' - \' || courses.name) ILIKE ?',
-                                            session[:semester],
-                                            '%' + params[:search] + '%'],
-                            :joins => :sections,
-                            :include => :sections,
-                            # Postgres requires you to explicitly list all fields to group. Whyyy? This is fixed by Rails 3 with :unique => true
-                            :group => Course.new.attributes.keys.map{|field| "courses.#{field}"}.join(", ") + ', courses.id',
-                            :order => "courses.subject || courses.number")
+    @courses = Course.where('semester_id = ? AND (courses.subject || courses.number || \' - \' || courses.name) ILIKE ?',
+                            session[:semester],
+                            '%' + params[:search] + '%')
+    @courses = @courses.joins(:sections).includes(:sections).order("courses.subject || courses.number")
+    @courses = @courses.group(Course.new.attributes.keys.map{|field| "courses.#{field}"}.join(", ") + ', courses.id')
+    @courses = @courses.all
 
-      # Load schedule so we don't display buttons for things already added
-      @registered_section_ids = RegisteredSection.all.map &:section_id
-      @wishlist_section_ids = WishlistSection.all.map &:section_id
+    # Load schedule so we don't display buttons for things already added
+    @registered_section_ids = RegisteredSection.all.map &:section_id
+    @wishlist_section_ids = WishlistSection.all.map &:section_id
 
-    rescue TypeError # when params[:search].nil?
-      # Don't search for courses if they haven't searched for them
-      # The view should display instructions if @courses.nil?
-      # and show that there were no results  if @courses.empty?
-      @courses = nil
-    end
+  rescue TypeError # when params[:search].nil?
+    # Don't search for courses if they haven't searched for them
+    # The view should display instructions if @courses.nil?
+    # and show that there were no results  if @courses.empty?
+    @courses = nil
   end
 
 end
