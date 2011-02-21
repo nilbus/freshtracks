7205536755a19f161f62e3d1301acf5bb5684a5b
Edward Anderson <nilbus@nilbus.com>
2011-02-21 11:05:02 -0500

    Switch development environment to postegresql

--- db/schema.rb
+++ db/schema.rb
@@ -51,6 +51,12 @@ ActiveRecord::Schema.define(:version => 20110216163958) do
     t.datetime "updated_at"
   end
 
+  create_table "passed_courses", :force => true do |t|
+    t.integer  "course_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "prerequisites", :force => true do |t|
     t.integer  "course_id"
     t.integer  "needs_id"
