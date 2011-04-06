32f5248d5f135db13ac0f9e3908f6f418c98c939
Ben Berry <bsberry@ncsu.edu>
2011-04-06 12:24:47 -0400

    Re-enable schedule button when you remove it from your schedule

--- app/controllers/registered_sections_controller.rb
+++ app/controllers/registered_sections_controller.rb
@@ -29,16 +29,14 @@ class RegisteredSectionsController < ApplicationController
 
   # unregister
   def destroy
-    RegisteredSection.find(params[:id]).destroy
+    @registered_section = RegisteredSection.find(params[:id])
+    @registered_section.destroy
     respond_to do |format|
       format.html do
         flash[:notice] = "Class successfully removed from your schedule."
         redirect_to wishlist_sections_path
       end
-      format.js do
-        # Row is removed with application.js makeRowRemover
-        render :text => ''
-      end
+      format.js #registered_sections/destroy.js.erb 
     end
   end
 
